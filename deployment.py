8414ecc33578f0d2736eb0e703aa8c2c1bf008b8
Mikel <mxagar@gmail.com>
2023-02-23 08:33:20 +0100

    started...

--- deployment.py
+++ deployment.py
@@ -1,27 +1,30 @@
-from flask import Flask, session, jsonify, request
+"""This module is in charge of deploying the already
+trained and scored model.
+
+The config.json file parametrizes the necessary paths.
+
+Author: Mikel Sagardia
+Date: 2023-02-20
+"""
+import os
+import pickle
+import json
 import pandas as pd
 import numpy as np
-import pickle
-import os
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-import json
+from flask import Flask, session, jsonify, request
 
-
-
-##################Load config.json and correct path variable
+# Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
 
-
-####################function for deployment
 def store_model_into_pickle(model):
     #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
 
+if __nanme__ == "__main__":
+    store_model_into_pickle()

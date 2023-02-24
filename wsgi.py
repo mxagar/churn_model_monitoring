a703b4c92e1842e0a3d26e3b45bb695c17fa3299
Mikel <mxagar@gmail.com>
2023-02-24 15:15:32 +0100

    docs

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
-from app import app
+"""This module imports the Flask API app
+and runs it.
 
+Author: Mikel Sagardia
+Date: 2023-02-20
+"""
+from app import app
 
 if __name__ == "__main__":
     app.run()

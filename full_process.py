067776f564a38d6c862dbc469218e90eedbfceb7
Mikel <mxagar@gmail.com>
2023-02-24 15:16:12 +0100

    first version

--- full_process.py
+++ full_process.py
@@ -1,40 +1,122 @@
+"""This module 
+"""
+import os
+import json
+import pandas as pd
 
-
+import ingestion
 import training
 import scoring
 import deployment
 import diagnostics
 import reporting
 
-##################Check and read new data
-#first, read ingestedfiles.txt
+# Load config.json and get path variables
+with open('config.json','r') as f:
+    config = json.load(f)
 
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+# "data/ingested/final_data.csv"
+dataset_csv_path = os.path.join(os.getcwd(),
+                                config['output_folder_path'],
+                                config['compiled_data_filename'])
+ # "data/development", "data/production"
+input_folder_path = os.path.join(os.getcwd(),
+                                 config['input_folder_path'])
+# "data/ingested/ingested_files.csv"
+ingestion_record_path = os.path.join(os.getcwd(),
+                                     config['output_folder_path'],
+                                     config['compilation_record_filename'])
+# "models/production/trained_model.pkl"
+model_path = os.path.join(os.getcwd(),
+                          config['prod_deployment_path'],
+                          config['model_filename'])
+# "data/test/test_data.csv"
+test_data_path = os.path.join(os.getcwd(),
+                              config['test_data_path'],
+                              config['test_data_filename'])
+features = config['features'] # ['lastmonth_activity', 'lastyear_activity', 'number_of_employees']
+target = config['target'] # 'exited'
 
+def check_ingested_data():
+    """Check that all the files from config.input_folder_path
+    have a record in data/ingested/ingested_files.csv.
+    If not, ingest new data and return True, else False.
+    """
+    # Load records of ingested files
+    records = pd.read_csv(ingestion_record_path)
+        
+    # Get files in source directory
+    filenames = os.listdir(input_folder_path)
 
+    # Check if all files in source directory
+    # have a record
+    all_in = False
+    for each_filename in filenames:
+        if each_filename.endswith(".csv"):
+            filepath = os.path.join(input_folder_path,
+                                    each_filename)
+            if not filepath in records['filepath'].to_list():
+                break
+        all_in = True
+    
+    if not all_in:
+        ingestion.merge_multiple_data()
 
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
+    return not all_in
 
+def check_model_drift():
+    """Check whether there is model drift:
+    
+    """
+    #check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
+    pass
 
-##################Checking for model drift
-#check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
+def check_data_drift():
+    """Check whether there is data drift:
+    That occurs when the distributions of the column values
+    change considerably.
+    """
+    # TODO: This needs to be implemented yet
+    # However, the most important function is check_model_drift()
+    # because the only relevant data drifts are those which
+    # cause a model drift!
+    # One way of implementing this is with a T-Test,
+    # diagnostics.dataframe_summary() provides with all the information
+    # to compute that.
+    return False
 
+def run_monitoring():
+    """Run monitoring:
+    - Check if there is new data; if so, continue
+    - Check if there is model or data drift; if so:
+        - Re-train
+        - Re-deploy
+        - Run reporting for the re-deployed model 
+        - Compute new score for the re-deployed model
+        - Run diagnostics for the re-deployed model
+    """
+    if check_ingested_data():
+        model_drift = False
+        data_drift = False
+        model_drift = check_model_drift()
+        data_drift = check_data_drift()
+        if model_drift or data_drift:
+            # Re-Train
+            training.train_model()
+            # Re-Deploy
+            deployment.store_model_into_pickle()
+            # Run Reporting
+            reporting.score_model()
+            # Compute New Score
+            _ = scoring.score_model()
+            # Run Diagnostics
+            diagnostics.run_diagnostics()
+            # Spin up diagnostics server
+            # NO
+        else:
+            print("No significant changes found; continuing with current model.")            
+    else:
+        print("No new data found; continuing with current model.")
 
-##################Deciding whether to proceed, part 2
-#if you found model drift, you should proceed. otherwise, do end the process here
-
-
-
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
-
-##################Diagnostics and reporting
-#run diagnostics.py and reporting.py for the re-deployed model
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    run_monitoring()

0e087d695e913682d1c3783663242de908041fc1
Mikel <mxagar@gmail.com>
2023-02-24 15:32:12 +0100

    re-arrange

--- api_calls.py
+++ api_calls.py
@@ -33,35 +33,42 @@ api_call_path = os.path.join(os.getcwd(),
                              config['output_model_path'],
                              config['api_call_filename'])
 
-# Specify base URL that resolves to our workspace
-URL = "http://127.0.0.1:8000"
-# Lists of all responses and endpoints
-endpoints = []
-responses = []
+def run():
+    """Main function in which all API calls
+    are run and where their responses are saved to file.
+    """
+    # Specify base URL that resolves to our workspace
+    URL = "http://127.0.0.1:8000"
+    # Lists of all responses and endpoints
+    endpoints = []
+    responses = []
 
-# Create endpoint calls
-endpoints.append(URL + "/prediction?filename=" + test_data_path) # data/test/test_data.csv
-endpoints.append(URL + "/scoring")
-endpoints.append(URL + "/summarystats") # HTML table
-endpoints.append(URL + "/diagnostics/timing")
-endpoints.append(URL + "/diagnostics/dependencies") # HTML table
+    # Create endpoint calls
+    endpoints.append(URL + "/prediction?filename=" + test_data_path) # data/test/test_data.csv
+    endpoints.append(URL + "/scoring")
+    endpoints.append(URL + "/summarystats") # HTML table
+    endpoints.append(URL + "/diagnostics/timing")
+    endpoints.append(URL + "/diagnostics/dependencies") # HTML table
 
-# Call endpoints and collect responses
-for i, endpoint in enumerate(endpoints):
-    # GET method, extract content, convert to string
-    response = requests.get(endpoint).content.decode("utf-8") 
-    if i == 2 or i == 4:
-        responses.append(pd.read_html(response)[0].drop("Unnamed: 0", axis=1).to_string())
-    else:
-        responses.append(response)
-    # Display
-    print("\n")
-    print(endpoint)
-    print(responses[-1])
-
-# Save responses to file
-with open(api_call_path, 'w') as f:
-    f.write("--- API Calls Report ---\n")
+    # Call endpoints and collect responses
     for i, endpoint in enumerate(endpoints):
-        f.write("\n" + endpoint + "\n")
-        f.write(responses[i] + "\n")
+        # GET method, extract content, convert to string
+        response = requests.get(endpoint).content.decode("utf-8") 
+        if i == 2 or i == 4:
+            responses.append(pd.read_html(response)[0].drop("Unnamed: 0", axis=1).to_string())
+        else:
+            responses.append(response)
+        # Display
+        print("\n")
+        print(endpoint)
+        print(responses[-1])
+
+    # Save responses to file
+    with open(api_call_path, 'w') as f:
+        f.write("--- API Calls Report ---\n")
+        for i, endpoint in enumerate(endpoints):
+            f.write("\n" + endpoint + "\n")
+            f.write(responses[i] + "\n")
+
+if __name__ == "__main__":
+    run()
